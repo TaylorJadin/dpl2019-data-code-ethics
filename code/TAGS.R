6fb8fc6f0394822fdca3569fb9e7f900fe370d2a
Taylor Jadin <jadin.taylor@gmail.com>
2019-08-08 15:58:47 -0400

    added TAGS

---
+++ code/TAGS.R
@@ -0,0 +1,9 @@
+TAGS %>%
+  filter(!is.na(from_user),
+         from_user!="DonaldBittar")%>%
+  count(from_user, sort = TRUE)%>%
+  slice(1:20)%>%
+  mutate(from_user = reorder(from_user,n))%>%
+  ggplot(mapping = aes(from_user,n))+
+  geom_col()+
+  coord_flip()
