98acde12fccf39353ded46dc3ab583f1219fa5fb
Taylor Jadin <jadin.taylor@gmail.com>
2019-08-09 09:46:51 -0400

    Update TAGS.R

--- code/TAGS.R
+++ code/TAGS.R
@@ -6,6 +6,6 @@ TAGS %>%
   count(from_user, sort = TRUE)%>%
   slice(1:20)%>%
   mutate(from_user = reorder(from_user,n))%>%
-  ggplot(mapping = aes(from_user,n))+
+  ggplot(mapping = aes(from_user,n color = from_user))+
   geom_col()+
   coord_flip()
